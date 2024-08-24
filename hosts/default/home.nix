77bc3eb2e2e41e5c1c1320ed4db311bdbbe23bf5
Dexter Hedman <dexterhedman05@proton.me>
2024-08-24 21:39:02 +0200

    Test without case-sensitive patch

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -3,7 +3,6 @@ let dmenu = pkgs.dmenu.override(
 {
     patches = [
         ./dmenu-patches/dmenu-center-20240616-36c3d68.diff
-        ./dmenu-patches/dmenu-caseinsensitive-5.0.diff
         ./dmenu-patches/dmenu-linesbelowprompt-and-fullwidth-20211014.diff
     ];
 });
