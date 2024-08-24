b4ea1ac8fc1b2e7dcff9a663481cf84da771624d
Dexter Hedman <dexterhedman05@proton.me>
2024-08-24 21:33:26 +0200

    Quick fix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -2,8 +2,8 @@
 let dmenu = pkgs.dmenu.override(
 {
     patches = [
-        ./dmenu-patches/dmenu-center-20240616-36c3d68.diff
         ./dmenu-patches/dmenu-caseinsensitive-5.0.diff
+        ./dmenu-patches/dmenu-center-20240616-36c3d68.diff
         ./dmenu-patches/dmenu-linesbelowprompt-and-fullwidth-20211014.diff
     ];
 });
