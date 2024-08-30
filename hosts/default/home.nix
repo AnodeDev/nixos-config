211fd63d920e163c0bb0ad2b39147a6648645cb5
Dexter Hedman <dexterhedman05@proton.me>
2024-08-30 20:14:50 +0200

    Changed mkDefault to mkForce

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -17,6 +17,7 @@ in
             "steam"
             "steam-original"
             "steam-run"
+            "spotify"
         ];
 
     home.packages = [
@@ -37,6 +38,7 @@ in
         pkgs.freetube
         pkgs.vesktop
         pkgs.steam
+        pkgs.spotify
 
         # Theming
         pkgs.bibata-cursors
@@ -54,20 +56,20 @@ in
 
     home.sessionVariables = {
         # XDG setup
-        XDG_DATA_HOME = lib.mkDefault "${config.home.homeDirectory}/.local/share";
-        XDG_STATE_HOME = lib.mkDefault "${config.home.homeDirectory}/.local/state";
-        XDG_CACHE_HOME = lib.mkDefault "${config.home.homeDirectory}/.cache";
-        XDG_CONFIG_HOME = lib.mkDefault "${config.home.homeDirectory}/.config";
+        XDG_DATA_HOME = lib.mkForce "${config.home.homeDirectory}/.local/share";
+        XDG_STATE_HOME = lib.mkForce "${config.home.homeDirectory}/.local/state";
+        XDG_CACHE_HOME = lib.mkForce "${config.home.homeDirectory}/.cache";
+        XDG_CONFIG_HOME = lib.mkForce "${config.home.homeDirectory}/.config";
 
         # Home directory cleaning
-        GTK2_RC_FILES = lib.mkDefault "${config.xdg.configHome}/gtk-2.0/gtkrc";
-        XCOMPOSECACHE = lib.mkDefault "${config.xdg.cacheHome}/X11/xcompose";
-        XCURSOR_PATH = lib.mkDefault "/usr/share/icons:${config.xdg.dataHome}/icons";
-        NIX_CONF_DIR = lib.mkDefault "${config.home.homeDirectory}/nixos";
+        GTK2_RC_FILES = lib.mkForce "${config.xdg.configHome}/gtk-2.0/gtkrc";
+        XCOMPOSECACHE = lib.mkForce "${config.xdg.cacheHome}/X11/xcompose";
+        XCURSOR_PATH = lib.mkForce "/usr/share/icons:${config.xdg.dataHome}/icons";
+        NIX_CONF_DIR = lib.mkForce "${config.home.homeDirectory}/nixos";
 
         # Languages
-        CARGO_HOME = lib.mkDefault "${config.home.homeDirectory}/.config/languages/cargo";
-        RUSTUP_HOME = lib.mkDefault "${config.home.homeDirectory}/.config/languages/rustup";
+        CARGO_HOME = lib.mkForce "${config.home.homeDirectory}/.config/languages/cargo";
+        RUSTUP_HOME = lib.mkForce "${config.home.homeDirectory}/.config/languages/rustup";
 
         # Other
         EDITOR = "nvim";
