2a9492a6afe673a3d1c5d7dccedc2cd3e06090fb
Dexter Hedman <dexterhedman05@proton.me>
2024-10-14 15:12:11 +0200

    Removed stupid mistake from harpoon.lua

--- hosts/default/nvim/plugins/harpoon.lua
+++ hosts/default/nvim/plugins/harpoon.lua
@@ -1,5 +1,4 @@
 local harpoon = require("harpoon")
-local plenary = require("plenary")
 local telescope = require("telescope")
 local map = vim.keymap
 local opts = { noremap = true, silent = true }
@@ -9,5 +8,4 @@ telescope.load_extension("harpoon")
 map.set("n", "<leader>ht", "<CMD>Telescope harpoon marks", opts)
 map.set("n", "<leader>ha", "<CMD>lua require('harpoon.mark').add_file()", opts)
 
-plenary.setup()
 harpoon.setup()
