0477896a5329ce787f3fb5b9d3214c414326e7e4
Dexter Hedman <dexterhedman05@proton.me>
2024-10-14 15:13:56 +0200

    Fixed another stupid mistake in harpoon.lua

--- hosts/default/nvim/plugins/harpoon.lua
+++ hosts/default/nvim/plugins/harpoon.lua
@@ -5,7 +5,7 @@ local opts = { noremap = true, silent = true }
 
 telescope.load_extension("harpoon")
 
-map.set("n", "<leader>ht", "<CMD>Telescope harpoon marks", opts)
-map.set("n", "<leader>ha", "<CMD>lua require('harpoon.mark').add_file()", opts)
+map.set("n", "<leader>ht", "<CMD>Telescope harpoon marks<CR>", opts)
+map.set("n", "<leader>ha", "<CMD>lua require('harpoon.mark').add_file()<CR>", opts)
 
 harpoon.setup()
