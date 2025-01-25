dc814c9ef1db74ecefe20b7e1219f5e91b2a3f72
Dexter Hedman <dexterhedman05@proton.me>
2025-01-25 23:50:35 +0100

    Switch to Wayland + Hyprland

--- modules/default.nix
+++ modules/default.nix
@@ -7,15 +7,13 @@
     ./nixos/sddm.nix
     ./nixos/xserver.nix
     ./nixos/main-user.nix
-    ./nixos/river.nix
-    ./nixos/sway.nix
+    ./nixos/hyprland.nix
   ];
 
-  xserver.enable = lib.mkDefault true;
-  bspwm.enable = lib.mkDefault true;
-  picom.enable = lib.mkDefault true;
+  xserver.enable = lib.mkDefault false;
+  bspwm.enable = lib.mkDefault false;
+  picom.enable = lib.mkDefault false;
   sddm.enable = lib.mkDefault true;
-  sddm.enableWayland = lib.mkDefault false;
-  river.enable = lib.mkDefault false;
-  sway.enable = lib.mkDefault false;
+  sddm.enableWayland = lib.mkDefault true;
+  hyprland.enable = lib.mkDefault true;
 }
