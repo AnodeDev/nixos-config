8f4b48e5df1b89db07bfa0fe62c942f2ace2fc28
Dexter Hedman <dexterhedman05@proton.me>
2025-02-11 00:00:43 +0100

    Fixed gpg config

--- home/programs/security/gpg.nix
+++ home/programs/security/gpg.nix
@@ -2,18 +2,18 @@
   programs.gpg = {
     enable = true;
     settings = {
-      "personal-digest-preferences" = "SHA512 SHA384 SHA256";
-      "default-preference-list" = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-      "require-secmem" = true;
-      "s2k-cipher-algo" = "AES256";
-      "s2k-digest-algo" = "SHA512";
-      "s2k-count" = 65011712;
-      "use-agent" = true;
-      "keyserver" = "hkps://keys.openpgp.org";
-      "keyserver-options" = "auto-key-retrieve";
-      "no-comments" = true;
-      "no-emit-version" = true;
-      "tofu-policy" = "good";
+      personal-digest-preferences = "SHA512 SHA384 SHA256";
+      default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+      require-secmem = true;
+      s2k-cipher-algo = "AES256";
+      s2k-digest-algo = "SHA512";
+      s2k-count = "65011712";
+      use-agent = true;
+      keyserver = "hkps://keys.openpgp.org";
+      keyserver-options = "auto-key-retrieve";
+      no-comments = true;
+      no-emit-version = true;
+      # tofu-policy = "good";
     };
   };
 
