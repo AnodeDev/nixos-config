0d8e088f2c9ee8d7aed2c7f48e60a8ac1738dda4
Dexter Hedman <dexterhedman05@proton.me>
2025-02-11 23:20:01 +0100

    Updated NVF Oil setup

--- modules/editors/neovim/plugins/oil.nix
+++ modules/editors/neovim/plugins/oil.nix
@@ -8,14 +8,100 @@
           default_file_explorer = true;
 
           columns = [
+            "icon"
             "permissions"
             "size"
             "mtime"
           ];
 
+          buf_options = {
+            buflisted = false;
+            bufhidden = "hide";
+          };
+
+          win_options = {
+            wrap = false;
+            signcolumn = "no";
+            cursorcolumn = false;
+            foldcolumn = "0";
+            spell = false;
+            list = false;
+            conceallevel = 3;
+            concealcursor = "nvic";
+          };
+
           delete_to_trash = false;
-          skip_confirm_for_simple_edits = false;
+          skip_confirm_for_simple_edits = true;
           prompt_save_on_select_new_entry = true;
+          
+          cleanup_delay_ms = 2000;
+          lsp_file_methods = {
+            timeout_ms = 1000;
+            autosave_changes = false;
+          };
+
+          # Options:
+          #   false
+          #   "editable"
+          #   "name"
+          constrain_cursor = "name";
+          watch_for_changes = false;
+
+          view_options = {
+            natural_order = true;
+            case_sensitive = false;
+            sort = [
+              [ "type" "asc" ]
+              [ "name" "asc" ]
+            ];
+          };
+
+          float = {
+            padding = 2;
+            max_width = 0;
+            max_height = 0;
+            border = "rounded";
+            win_options = {
+              winblend = 0;
+            };
+            preview_split = "right";
+          };
+
+          preview = {
+            max_width = 0.9;
+            min_width = [ 40 0.4 ];
+            width = "nil";
+            max_height = 0.9;
+            min_height = [ 5 0.1 ];
+            height = "nil";
+            border = "rounded";
+            win_options = {
+              winblend = 0;
+            };
+            update_on_cursor_moved = true;
+          };
+
+          progress = {
+            max_width = 0.9;
+            min_width = [ 40 0.4 ];
+            width = "nil";
+            max_height = [ 10 0.9 ];
+            min_height = [ 5 0.1 ];
+            height = "nil";
+            border = "rounded";
+            minimized_border = "none";
+            win_options = {
+              winblend = 0;
+            };
+          };
+
+          ssh = {
+            border = "rounded";
+          };
+
+          keymaps_help = {
+            border = "rounded";
+          };
         };
         
         lazy = true;
