3fca631ff7e484636f15e26ec32d927b1c6868c9
Dexter Hedman <dexterhedman05@proton.me>
2025-02-11 23:44:44 +0100

    Removed unecessary settings from Oil in NVF config

--- modules/editors/neovim/plugins/oil.nix
+++ modules/editors/neovim/plugins/oil.nix
@@ -56,45 +56,6 @@
             ];
           };
 
-          float = {
-            padding = 2;
-            max_width = 0;
-            max_height = 0;
-            border = "rounded";
-            win_options = {
-              winblend = 0;
-            };
-            preview_split = "right";
-          };
-
-          preview = {
-            max_width = 0.9;
-            min_width = [ 40 0.4 ];
-            width = "nil";
-            max_height = 0.9;
-            min_height = [ 5 0.1 ];
-            height = "nil";
-            border = "rounded";
-            win_options = {
-              winblend = 0;
-            };
-            update_on_cursor_moved = true;
-          };
-
-          progress = {
-            max_width = 0.9;
-            min_width = [ 40 0.4 ];
-            width = "nil";
-            max_height = [ 10 0.9 ];
-            min_height = [ 5 0.1 ];
-            height = "nil";
-            border = "rounded";
-            minimized_border = "none";
-            win_options = {
-              winblend = 0;
-            };
-          };
-
           ssh = {
             border = "rounded";
           };
