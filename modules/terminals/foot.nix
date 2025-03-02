a50eec9a26a1378684be6b9ab104f9cd31edf0f8
Dexter Hedman <dexterhedman05@proton.me>
2025-03-02 11:45:03 +0100

    Added Foot configuration

--- modules/terminals/foot.nix
+++ modules/terminals/foot.nix
@@ -4,5 +4,20 @@
     package = pkgs.foot;
 
     theme = "catppuccin-mocha";
+    settings = {
+      main = {
+        font = "Maple Mono NF:size=11";
+        dpi-aware = "yes";
+        pad = "10x10";
+        underline-offset = 2;
+      };
+      scrollback = {
+        lines = 5000;
+      };
+      cursor = {
+        style = "block";
+        blink = "no";
+      };
+    };
   };
 }
