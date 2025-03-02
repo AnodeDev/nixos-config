99a7b5b3322cf821b1a76b89d478a07fb6c120a5
Dexter Hedman <dexterhedman05@proton.me>
2025-03-02 22:20:31 +0100

    Tweaked Hyprland animations

--- home/programs/wayland/hyprland/animations.nix
+++ home/programs/wayland/hyprland/animations.nix
@@ -1,8 +1,19 @@
 {
   wayland.windowManager.hyprland.settings = {
     animation = [
-      "workspaces, 1, 8, default"
-      "windows, 1, 10, default"
+      # Windows
+      "windowsIn, 1, 5, easeOutCirc"
+      "windowsMove, 1, 7, easeOutExpo"
+      "windowsOut, 1, 7, easeOutQuad"
+
+      # Workspaces
+      "workspaces, 1, 6, default"
+    ];
+
+    bezier = [
+      "easeOutCirc, 0, 0.55, 0.45, 1"
+      "easeOutQuad, 0.5, 1, 0.89, 1"
+      "easeOutExpo, 0.16, 1, 0.3, 1"
     ];
   };
 }
