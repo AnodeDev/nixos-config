8ea5b29ac2c7f778fa80ba986d1e2cac5c292e81
Dexter Hedman <dexterhedman05@proton.me>
2025-04-03 18:15:49 +0200

    Improved FreeTube config

--- home/programs/desktop/freetube.nix
+++ home/programs/desktop/freetube.nix
@@ -3,8 +3,19 @@
     enable = true;
     package = pkgs.freetube;
     settings = {
-      defaultQuality = "1440";
+      # Theme
       baseTheme = "catppuccinMocha";
+      mainColor = "CatppuccinMochaPink";
+      secColor = "CatppuccinMochaGreen";
+
+      # Player
+      defaultQuality = "1440";
+      useSponsorBlock = true;
+      defaultViewingMode = "theatre";
+
+      # UI
+      hideTrendingVideos = true;
+      hidePopularVideos = true;
     };
   };
 }
