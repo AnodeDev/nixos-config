1c821bfc194349a47f9f4503306cbfe328e8c77a
Dexter Hedman <dexterhedman05@proton.me>
2025-04-18 17:28:04 +0200

    Added River settings

--- home/programs/wayland/river/default.nix
+++ home/programs/wayland/river/default.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: {
+  imports = [
+    ./settings.nix
+  ];
+
   wayland.windowManager.river = {
     enable = true;
     package = pkgs.river;
---
+++ home/programs/wayland/river/settings.nix
@@ -0,0 +1,9 @@
+{
+  wayland.windowManager.river.settings = {
+    map = [
+      "normal Super Return spawn ghostty"
+      "normal Super C close"
+      "normal Super P spawn tofi-drun --drun-launch=true"
+    ];
+  };
+}
