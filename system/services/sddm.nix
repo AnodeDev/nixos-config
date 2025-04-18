e523dedcb3dbb8b08745373432700216c2193fe1
Dexter Hedman <dexterhedman05@proton.me>
2025-04-18 23:44:20 +0200

    Added River to UWSM

--- system/services/sddm.nix
+++ system/services/sddm.nix
@@ -25,10 +25,17 @@
 
   programs.uwsm = {
     enable = true;
-    waylandCompositors.hyprland = {
-      prettyName = "Hyprland";
-      comment = lib.mkForce "An intelligent tiling compositor";
-      binPath = "/run/current-system/sw/bin/Hyprland";
+    waylandCompositors = {
+      hyprland = {
+        prettyName = "Hyprland";
+        comment = lib.mkForce "An intelligent tiling compositor";
+        binPath = "/run/current-system/sw/bin/Hyprland";
+      };
+      river = {
+        prettyName = "River";
+        comment = lib.mkForce "A minimal tiling compositor";
+        binPath = "/run/current-system/sw/bin/river";
+      };
     };
   };
 }
