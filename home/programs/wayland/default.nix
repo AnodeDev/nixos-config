1d5274633fa3e5fe7a9ca777f792cc73bb357c0f
Dexter Hedman <dexterhedman05@proton.me>
2025-04-18 17:23:54 +0200

    Added River module

--- home/programs/wayland/default.nix
+++ home/programs/wayland/default.nix
@@ -6,12 +6,17 @@
 }: {
   imports = [
     ./hyprland
+    ./river
   ];
 
   home.packages = with pkgs; [
     wl-clipboard
     clipse
     wlr-randr
+    wlopm
+    way-displays
+    swww
+    waylock
   ];
 
   systemd.user.targets.tray.Unit.Requires = lib.mkForce ["graphical-session.target"];
