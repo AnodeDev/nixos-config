66989b5c55a89a80a850aed66eaaff799679013e
Dexter Hedman <dexterhedman05@proton.me>
2025-05-13 21:30:12 +0200

    Improved River config

--- home/programs/wayland/river/settings.nix
+++ home/programs/wayland/river/settings.nix
@@ -6,28 +6,16 @@
     };
 
     settings = {
-      border-color-focused = lib.mkForce "0xa6e3a1";
-      border-color-unfocused = lib.mkForce "0x1e1e2e";
+      border-color-focused = lib.mkForce "0xcdd6f4";
+      border-color-unfocused = lib.mkForce "0xcdd6f4";
+      border-width = 2;
+
+      # Enables Server Side Decorations (SSD)
+      rule-add = [
+        "-app-id '*' ssd"
+      ];
 
       default-layout = "rivertile";
-
-      tag-label = [
-        "1 Main 1"
-        "2 Main 2"
-        "3 Main 3"
-        "4 Main 4"
-        "5 Main 5"
-        "6 Ext 1"
-        "7 Ext 2"
-        "8 Ext 3"
-        "9 Ext 4"
-        "10 Ext 5"
-      ];
-
-      output = [
-        "DP-1 tags 1 2 3 4 5"
-        "HDMI-A-1 tags 6 7 8 9 10"
-      ];
 
       focus-output = "DP-1";
       set-focused-tags = 1;
@@ -56,12 +44,6 @@
         "normal $mod+Shift period send-to-output next"
 
         "normal $mod R enter-mode resize"
-
-        "resize N resize horizontal -20"
-        "resize E resize vertical 20"
-        "resize I resize vertical -20"
-        "resize O resize horizontal 20"
-        "resize Escape enter-mode normal"
       ];
     };
 
@@ -69,9 +51,9 @@
       way-displays &
       mako &
       swww-daemon &
-      swww img -o "HDMI-A-1" ~/Media/Wallpapers/pixel-sky.png
-      swww img -o "DP-1" ~/Media/Wallpapers/pixel-sky.png
+      swww img ~/Media/Wallpapers/dragon-slayer.png
       rivertile -view-padding 6 -outer-padding 6 &
+      riverctl hide-cursor when-typing enabled
 
       for i in $(seq 1 9); do
         riverctl map normal $mod $i set-focused-tags $((1 << ($i - 1)))
