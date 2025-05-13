06e8018e0e041314232051a6261954fdbde8219a
Dexter Hedman <dexterhedman05@proton.me>
2025-05-13 21:30:38 +0200

    Improved Wofi config

--- home/programs/wayland/wofi.nix
+++ home/programs/wayland/wofi.nix
@@ -1,18 +1,33 @@
-{pkgs, ...}: {
+{lib, pkgs, ...}: {
   programs.wofi = {
     enable = true;
     package = pkgs.wofi;
 
-    settings = {
-      location = "center";
+    settings = lib.mkForce {
+      location = "top_left";
+      width = 800;
+      monitor = "DP-1";
     };
 
-    style = ''
+    style = lib.mkForce ''
       window {
+        font-family = "Maple Mono NF";
+        font-size = 12px;
+
         margin = 5px;
         border = 1px solid #cdd6f4;
         background-color = #181825;
-      };
+      }
+
+      #input {
+        margin: 5px;
+        border: 1px solid #f9e2af;
+        background-color: #1e1e2e;
+      }
+
+      #entry {
+        background-color: #181825;
+      }
     '';
   };
 }
