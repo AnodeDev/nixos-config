d9a475085d1e32f867f16f95d280021c7d52365a
Dexter Hedman <dexterhedman05@proton.me>
2025-05-22 22:04:22 +0200

    Improved MPD config

--- home/services/media/mpd.nix
+++ home/services/media/mpd.nix
@@ -3,12 +3,22 @@
     enable = true;
     package = pkgs.mpd;
 
-    musicDirectory = "${config.home.homeDirectory}";
+    musicDirectory = "${config.home.homeDirectory}/Media/Music";
+    playlistDirectory = "${config.home.homeDirectory}/Media/Playlists";
+    dbFile = "${config.xdg.cacheHome}/mpd/mpd.db";
 
     network = {
       startWhenNeeded = true;
       port = 6600;
       listenAddress = "127.0.0.1";
     };
+
+    extraConfig = ''
+      log_file  "~/.local/cache/mpd/mpd.log"
+      audio_output {
+        type    "jack"
+        name    "PCH"
+      }
+    '';
   };
 }
